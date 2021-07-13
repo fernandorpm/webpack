57a6f6dad91ba3a24de05640686fcac6517587e4
Fernando Marques <frpm1001@gmail.com>
2021-07-13 20:17:31 -0300

    Add webpack default

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+   entry: {
+     index: './src/index.js',
+   },
+
+   devServer: {
+    contentBase: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+     publicPath: '/'
+   },
+ };
